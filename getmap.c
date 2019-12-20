52027a3ebd9559fd082213195aba0fee845599f8
sleng17 <sammert17@yandex.ru>
2019-12-21 01:27:06 +0300

    its working but only with one link per room yet

--- getmap.c
+++ getmap.c
@@ -1,6 +1,7 @@
+#include <string.h>
 #include "vizu.h"
 
-static void	get_ants(const int fd, t_data *data)
+void get_ants(const int fd, t_data *data)
 {
 	char *str;
 
@@ -25,16 +26,15 @@ static t_room	get_room(char *str)
 
 	iter = ft_strchr(str, ' ');
 	*iter = '\0';
-	room.name = ft_strdup(str);
+	room.name = strdup(str);
 	str = iter + 1;
 	room.x = ft_atoi(str);
 	str = ft_strchr(str, ' ') + 1;
 	room.y = ft_atoi(str);
-	room.links = vec_init(0, 0, sizeof(t_room*));
 	return (room);
 }
 
-static char	*get_rooms(const int fd, t_data *data)
+static char*	get_rooms(const int fd, t_data *data)
 {
 	char *str;
 	t_room	room;
@@ -66,33 +66,38 @@ static char	*get_rooms(const int fd, t_data *data)
 	return (str);
 }
 
-
-static void	get_links(const int fd, t_data *data, char *str)
+static	void get_links(const int fd, t_data *data, char *str)
 {
-	char *room1_name;
-	char *room2_name;
-	size_t	room1pos;
-	size_t	room2pos;
+	char *name1;
+	char *name2;
+	ssize_t pos1 = -1;
+	size_t pos2;
 
+	for (size_t i = 0; i < vec_size(data->rooms); ++i)
+		data->rooms[i].links = NULL;
 	while (TRUE)
 	{
 		if (*str != '#')
 		{
-			room2_name = ft_strchr(str, '-') + 1;
-			room2_name[-1] = '\0';
-			room1_name = str;
+			name1 = str;
+			name2 = ft_strchr(str, '-') + 1;
+			name2[-1] = '\0';
 			for (size_t i = 0; i < vec_size(data->rooms); ++i)
 			{
-				if (ft_strequ(data->rooms[i].name, room1_name))
-					room1pos = i;
-				else if (ft_strequ(data->rooms[i].name, room2_name))
-					room2pos = i;
+				if (ft_strequ(data->rooms[i].name, name1))
+					pos1 = i;
+				else if (ft_strequ(data->rooms[i].name, name2))
+					pos2 = i;
 			}
-//			printf("address1: %p\n", data->rooms + room2pos);
-//			printf("name: %s\n", data->rooms[room2pos].name);
-			vec_pushback(&(data->rooms[room1pos].links), &data->rooms[room2pos]);
-			vec_pushback(&(data->rooms[room2pos].links), &data->rooms[room1pos]);
-
+			if (pos1 >= 0) {
+				if (!data->rooms[pos1].links)
+					data->rooms[pos1].links = malloc(sizeof(t_data *));
+				data->rooms[pos1].links[0] = data->rooms + pos2;
+				if (!data->rooms[pos2].links)
+					data->rooms[pos2].links = malloc(sizeof(t_data *));
+				data->rooms[pos2].links[0] = data->rooms + pos1;
+			}
+			pos1 = -1;
 		}
 		free(str);
 		if (get_next_line(fd, &str) != 1)
@@ -103,8 +108,8 @@ static void	get_links(const int fd, t_data *data, char *str)
 t_data		get_map(const char *file)
 {
 	t_data	data;
-	char	*str;
 	int		fd;
+	char 	*str;
 
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
--- main.c
+++ main.c
@@ -32,23 +32,33 @@ int main(int argc, const char **argv)
 	// wnd = wnd_init();
 	data = get_map(argv[1]);
 
-	for (size_t i = 0; i < vec_size(data.rooms); ++i)
-	{
+	for (size_t i = 0; i < vec_size(data.rooms); ++i) {
 		printf("%s: %d - %d\n", data.rooms[i].name, data.rooms[i].x, data.rooms[i].y);
-		for (size_t j = 0; j < vec_size(data.rooms[i].links); ++j)
-			printf("|------>  %s\n", data.rooms[i].links[j]->name);
-		free(data.rooms[i].name);
-		vec_clear(&data.rooms[i].links);
+		if (data.rooms[i].links) {
+			printf("-----> %s\n", data.rooms[i].links[0]->name);
+		}
 	}
-
+	for (size_t i = 0; i < vec_size(data.rooms); ++i) {
+		free(data.rooms[i].name);
+		if (data.rooms[i].links)
+			free(data.rooms[i].links);
+	}
 	vec_clear(&data.rooms);
+
+
+//	for (size_t i = 0; i < vec_set_size(data.rooms); ++i)
+//	{
+//		printf("%s: %d - %d\n", data.rooms[i].name, data.rooms[i].x, data.rooms[i].y);
+//		printf("|------>  %s\n", data.rooms[i].links[0]->name);
+//		free(data.rooms[i].name);
+//	}
+
+//	vec_clear(&data.rooms);
 	// mlx_put_image_to_window(wnd.mlxptr, wnd.wndptr, wnd.imgptr, 0, 0);
 
 	// mlx_hook(wnd.wndptr, 2, 1L << 0, key_press, &wnd);
 	// mlx_hook(wnd.wndptr, 17, 1L << 17, vizu_exit, &wnd);
 	// mlx_loop(wnd.mlxptr);
-	if (argc && !argv)
-		return (0);
 
 	return (0);
 }
