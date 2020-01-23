646806e6567b2b4d90a13e3e11b5fa96d0ab2297
Daniil Naumenko <enshertid@icloud.com>
2020-01-23 21:59:43 +0300

    1

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -6,7 +6,7 @@
 /*   By: ymanilow <ymanilow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/18 19:07:02 by ymanilow          #+#    #+#             */
-/*   Updated: 2020/01/21 15:25:07 by ymanilow         ###   ########.fr       */
+/*   Updated: 2020/01/23 21:56:24 by ymanilow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,84 @@
 #define LEM_IN
 
 #include "libft.h"
-#include "graph.h"
-#include "hash_table.h"
-#include "turn.h"
-#include "ways.h"
 
-typedef struct				s_flags
+# define START			MIN_INT
+# define FINISH			-1
+
+
+
+typedef struct			s_iters
 {
-	t_bool					flag_ants;
-	t_bool					flag_links;
-	t_bool					flag_start;
-	t_bool					flag_end;
-	t_bool					flag_room;
-}							t_flags;
+	int					col;
+	int					iter;
+}						t_iters;
 
-typedef struct				s_pars
+typedef struct			s_coord
 {
-	int						fd;
-	int						hash_f;
-	int						hash_s;
-	t_room					*room_f;
-	t_room					*room_s;
-	char					*line;
-	char					**str;
-}							t_pars;
+	int					x;
+	int					y;
+}						t_coord;
 
-typedef struct				s_data
+typedef struct			s_flags
 {
-	t_graph					graph;
-	t_flags					flags;
-	t_hash_table			hash;
-	t_turn					turn;
-	t_pars					pars;
-	t_ways					ways;
-	t_ways					ways_for_algo;
-	int						ants;
-}							t_data;
+	t_bool				flag_end;
+	t_bool				flag_link;
+	t_bool				flag_ants;
+	t_bool				flag_room;
+	t_bool				flag_start;
+	t_bool				flag_not_error_link1;
+	t_bool				flag_not_error_link2;
+}						t_flags;
 
-void						parsing(t_data *data, char **av);
-void		 				suurballe_algo(t_data *data);
-int							get_next_line(const int fd, char **line);
 
-#endif
+typedef struct			s_room
+{
+	char				*name;
+	t_iters				iters;
+	t_coord				coord;
+	t_bool				ant_presence;
+	t_bool				link_presence;
+	int					num_of_room;
+	int					num_of_prev_room;
+	int					num_on_the_search;
+	struct s_room		**links;
+}						t_room;
+
+typedef struct			s_ways
+{
+	int					iter;
+	int					col;
+	t_room				*way;
+}						t_ways;
+
+typedef struct			s_turn_array
+{
+	t_room				**turn_array;
+	int					col;
+	int					iter;
+}						t_turn_array;
+
+typedef struct			s_data
+{
+	t_room				*rooms;
+	t_turn_array		turn;
+	t_room				end;
+	t_room				tmp;
+	t_flags				flags;
+	t_iters				iters;
+	int					ants;
+	int					fd;
+}						t_data;
+
+void					ft_parsing(char **av, t_data *data);
+
+void					ft_pars_rooms(t_data *data);
+void					ft_check_room(t_data *data, char **line);
+void					ft_check_links(t_data *data, char **line);
+void					ft_check_end(t_data *data, char **line);
+void					ft_check_start(t_data *data, char **line);
+void					pop_front(t_data *data);
+void					*ft_malloc_store(size_t size);
+void 					ft_free_storage();
+
+#endif //LEM_IN
